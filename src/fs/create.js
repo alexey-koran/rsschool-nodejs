a7450d514c1137876f4e349b8d7dfc5891bc8ec2
Alexey Koran <alexey.koran@gmail.com>
2024-10-08 02:10:25 +0400

    feat: implement create.js func

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { open } from 'node:fs/promises';
+
+const filePath = fileURLToPath(import.meta.url);
+const currentFolderPath = dirname(filePath);
+
+const fileContent = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+  let file;
+  try {
+    file = await open(`${currentFolderPath}/files/fresh.txt`, 'wx+');
+
+    file.write(fileContent);
+
+    file.close();
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
