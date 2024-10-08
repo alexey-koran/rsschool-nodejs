ab07542b96164b1828909927f0a0b0e761077c7d
Alexey Koran <alexey.koran@gmail.com>
2024-10-08 06:54:55 +0400

    feat: implement `calcHash.js` task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createReadStream } from 'node:fs';
+import { dirname } from 'node:path';
+import { stdout } from 'node:process';
+import { fileURLToPath } from 'node:url';
+
 const calculateHash = async () => {
-  // Write your code here
+  const { createHash } = await import('node:crypto');
+
+  const filePath = fileURLToPath(import.meta.url);
+  const currentFolderPath = dirname(filePath);
+
+  const hash = createHash('sha256');
+
+  const input = createReadStream(`${currentFolderPath}/files/fileToCalculateHashFor.txt`);
+
+  input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
