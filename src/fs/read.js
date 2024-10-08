2318224be021bae6b4dc6f3d3d6443a0f5f7fd4f
Alexey Koran <alexey.koran@gmail.com>
2024-10-08 04:39:28 +0400

    feat: implement `read.js` func

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { readFile } from 'node:fs/promises';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const filePath = fileURLToPath(import.meta.url);
+const currentFolderPath = dirname(filePath);
+
 const read = async () => {
-  // Write your code here
+  try {
+    const fileContent = await readFile(`${currentFolderPath}/files/fileToRead.txt`);
+    console.debug(fileContent.toString());
+  } catch (error) {
+    if (error?.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+
+    throw new Error(error);
+  }
 };
 
 await read();
