a43cc1716c50d28f6b111607a56032accc3a6359
Alexey Koran <alexey.koran@gmail.com>
2024-10-08 04:30:42 +0400

    feat: implement `delete.js` func

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { rm } from 'node:fs/promises';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const filePath = fileURLToPath(import.meta.url);
+const currentFolderPath = dirname(filePath);
+
 const remove = async () => {
-  // Write your code here
+  try {
+    await rm(`${currentFolderPath}/files/fileToRemove.txt`);
+  } catch (error) {
+    if (error?.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+
+    throw new Error(error);
+  }
 };
 
 await remove();
