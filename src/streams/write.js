9c9d0038cce23629fb965234fafde9ca5dce1165
Alexey Koran <alexey.koran@gmail.com>
2024-10-08 07:21:25 +0400

    feat: implement streams `write.js` task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import { createWriteStream } from 'node:fs';
+import { dirname } from 'node:path';
+import { stdin } from 'node:process';
+import { fileURLToPath } from 'node:url';
+
+const filePath = fileURLToPath(import.meta.url);
+const currentFolderPath = dirname(filePath);
+
 const write = async () => {
-  // Write your code here
+  const writeableStream = createWriteStream(`${currentFolderPath}/files/fileToWrite.txt`);
+
+  stdin.on('data', (data) => {
+    writeableStream.write(data);
+  });
+
+  stdin.on('end', () => {
+    writeableStream.end();
+  });
 };
 
 await write();
