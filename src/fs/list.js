ffb3eb0f545259409d30c51b52d0c73129eaeff4
Alexey Koran <alexey.koran@gmail.com>
2024-10-08 04:35:05 +0400

    feat: implement `list.js` func

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { readdir } from 'node:fs/promises';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const filePath = fileURLToPath(import.meta.url);
+const currentFolderPath = dirname(filePath);
+
 const list = async () => {
-  // Write your code here
+  try {
+    const list = await readdir(`${currentFolderPath}/files`);
+    console.debug(list);
+  } catch (error) {
+    if (error?.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+
+    throw new Error(error);
+  }
 };
 
 await list();
