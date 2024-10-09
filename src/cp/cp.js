f1686ff71c17877017c8cc895938162aeb4374c5
Alexey Koran <alexey.koran@gmail.com>
2024-10-09 05:30:33 +0400

    feat: implement cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import { fork } from 'node:child_process';
+import { dirname } from 'node:path';
+import { stdin, stdout } from 'node:process';
+import { fileURLToPath } from 'node:url';
+
+const filePath = fileURLToPath(import.meta.url);
+const currentFolderPath = dirname(filePath);
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const controller = new AbortController();
+
+  const childProcess = fork(`${currentFolderPath}/files/script.js`, [...args], { silent: true });
+
+  stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(stdout);
+
+  childProcess.on('error', (err) => {
+    throw new Error(err);
+  });
+
+  controller.abort();
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['test1', 'test2', 'test3', 'test4', 'test5']);
