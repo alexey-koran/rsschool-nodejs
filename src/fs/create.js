dd535dee2d10c2dc6fd7bc7ee0106085aa09c814
Alexey Koran <alexey.koran@gmail.com>
2024-10-14 02:39:15 +0400

    fix: fs create task, use `writeFile` from `node:fs/promises`

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,16 +1,10 @@
-import { open } from 'node:fs/promises';
+import { writeFile } from 'node:fs/promises';
 
 const fileContent = 'I am fresh and young';
 
 const create = async () => {
-  let file;
-
   try {
-    file = await open(`${import.meta.dirname}/files/fresh.txt`, 'wx+');
-
-    await file.write(fileContent);
-
-    await file.close();
+    await writeFile(`${import.meta.dirname}/files/fresh.txt`, fileContent, { flag: 'wx' });
   } catch (_error) {
     throw new Error('FS operation failed');
   }
