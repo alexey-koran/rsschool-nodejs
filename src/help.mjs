2f1916b7ac49f31ee27b21192903cca8842b349f
Alexey Koran <alexey.koran@gmail.com>
2024-10-15 02:29:44 +0400

    feat: add `commandsHelp` obj

---
+++ src/help.mjs
@@ -0,0 +1,118 @@
+export const commandsHelp = {
+  up: {
+    command: 'up',
+    description: {
+      text: 'Go upper from current directory',
+      hint: "(when you are in the root folder this operation shouldn't change working directory)",
+    },
+  },
+  cd: {
+    command: 'cd path_to_directory',
+    description: {
+      text: 'Go to dedicated folder from current directory',
+      hint: '(path_to_directory can be relative or absolute)',
+    },
+  },
+  ls: {
+    command: 'ls',
+    description: {
+      text: 'Print in console list of all files and folders in current directory',
+    },
+  },
+  cat: {
+    command: 'cat path_to_file',
+    description: {
+      text: "Read file and print it's content in console",
+    },
+  },
+  add: {
+    command: 'add new_file_name',
+    description: {
+      text: 'Create empty file in current working directory',
+    },
+  },
+  rn: {
+    command: 'rn path_to_file new_filename',
+    description: {
+      text: 'Rename file',
+      hint: '(content will remain unchanged)',
+    },
+  },
+  cp: {
+    command: 'cp path_to_file path_to_new_directory',
+    description: {
+      text: 'Copy file',
+    },
+  },
+  mv: {
+    command: 'mv path_to_file path_to_new_directory',
+    description: {
+      text: 'Move file',
+    },
+  },
+  rm: {
+    command: 'rm path_to_file',
+    description: {
+      text: 'Delete file',
+    },
+  },
+  os: {
+    command: 'os',
+    options: {
+      flag: {
+        eol: {
+          command: 'os --EOL',
+          description: {
+            text: 'Get EOL (default system End-Of-Line) and print it to console',
+          },
+        },
+        cpus: {
+          command: 'os --cpus',
+          description: {
+            text: 'Get host machine CPUs info and print it to console',
+            hint: '(overall amount of CPUS plus model and clock rate (in GHz) for each of them)',
+          },
+        },
+        homedir: {
+          command: 'os --homedir',
+          description: {
+            text: 'Get home directory and print it to console',
+          },
+        },
+        username: {
+          command: 'os --username',
+          description: {
+            text: 'Get current system user name and print it to console',
+          },
+        },
+        architecture: {
+          command: 'os --architecture',
+          description: {
+            text: 'Get CPU architecture for which Node.js binary has compiled and print it to console',
+          },
+        },
+      },
+    },
+    description: {
+      text: 'Operating system info',
+    },
+  },
+  hash: {
+    command: 'hash path_to_file',
+    description: {
+      text: 'Calculate hash for file and print it into console',
+    },
+  },
+  compress: {
+    command: 'compress path_to_file path_to_destination',
+    description: {
+      text: 'Compress file',
+    },
+  },
+  decompress: {
+    command: 'decompress path_to_file path_to_destination',
+    description: {
+      text: 'Decompress file',
+    },
+  },
+};
