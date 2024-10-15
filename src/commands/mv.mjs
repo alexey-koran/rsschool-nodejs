b953f221adb49d09542181fafb2f5dbeecdd968d
Alexey Koran <alexey.koran@gmail.com>
2024-10-15 21:13:20 +0400

    feat: implement `mv.mjs`

---
+++ src/commands/mv.mjs
@@ -0,0 +1,24 @@
+import { cp, rm } from 'node:fs/promises';
+import { join } from 'node:path';
+
+import { validatePath } from '../utils.mjs';
+
+export const mv = async ({ passedProps, currentWorkingDirectory }) => {
+  const pathToFile = passedProps[0];
+  const pathToNewDirectory = passedProps[1];
+
+  const sourcePath = join(currentWorkingDirectory, pathToFile);
+  const destinationPath = join(currentWorkingDirectory, pathToNewDirectory);
+
+  await validatePath(sourcePath);
+  await validatePath(destinationPath);
+
+  await cp(sourcePath, destinationPath, {
+    errorOnExist: true,
+    force: false,
+    recursive: true,
+    preserveTimestamps: true,
+  });
+
+  await rm(sourcePath);
+};
