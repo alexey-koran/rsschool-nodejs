ac34f4125b5180aae76d89f19e49857a11c01295
Alexey Koran <alexey.koran@gmail.com>
2024-10-15 09:19:28 +0400

    feat: implement `rm.mjs`

--- src/commands/rm.mjs
+++ src/commands/rm.mjs
@@ -1,5 +1,19 @@
 import { rm as fsRm } from 'node:fs/promises';
+import { homedir } from 'node:os';
+import { join } from 'node:path';
 
-export const rm = async ({ passedProps: pathToFile }) => {
-  await fsRm(pathToFile);
+import { programErrors } from '../messages.mjs';
+import { validatePath } from '../utils.mjs';
+
+export const rm = async ({ passedProps: pathToFile, currentWorkingDirectory }) => {
+  const newPath = join(currentWorkingDirectory, pathToFile);
+  const homeDir = homedir();
+
+  if (!newPath.startsWith(homeDir)) {
+    throw new Error(programErrors.outOfRootDirectory);
+  }
+
+  await validatePath(newPath);
+
+  await fsRm(newPath);
 };
