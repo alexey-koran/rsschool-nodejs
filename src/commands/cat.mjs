e5cdb6c296ea2b0da4f0d50a16858a39a03f46f8
Alexey Koran <alexey.koran@gmail.com>
2024-10-15 05:59:50 +0400

    feat: implement `cat.mjs`

---
+++ src/commands/cat.mjs
@@ -0,0 +1,24 @@
+import { readFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+
+import { programErrors } from '../messages.mjs';
+
+export const cat = async (pathToFile, flags) => {
+  if (flags && flags.length > 0) {
+    throw new Error(`${programErrors.invalidInput}. \nCat command does not support flags.`);
+  }
+
+  if (!pathToFile || pathToFile.length === 0) {
+    throw new Error(`${programErrors.invalidInput}. \nPath to file is not specified.`);
+  }
+
+  try {
+    const fileContent = await readFile(resolve(pathToFile), {
+      encoding: 'utf8',
+    });
+
+    console.debug(fileContent);
+  } catch (error) {
+    throw new Error(`${programErrors.operationFailed}. \n${error}`);
+  }
+};
