a0807f525c589a15b8f45c9e91d6ffe11263cbbb
Alexey Koran <alexey.koran@gmail.com>
2024-10-15 20:58:46 +0400

    feat: implement `compress.mjs`, `decompress.mjs`

---
+++ src/commands/compress.mjs
@@ -0,0 +1,24 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { join } from 'node:path';
+import { pipeline } from 'node:stream/promises';
+import { createBrotliCompress } from 'node:zlib';
+
+import { validatePath } from '../utils.mjs';
+
+export const compress = async ({ passedProps, currentWorkingDirectory }) => {
+  const pathToFile = passedProps[0];
+  const pathToDestination = passedProps[1];
+
+  const sourcePath = join(currentWorkingDirectory, pathToFile);
+  const destinationPath = join(currentWorkingDirectory, pathToDestination);
+
+  await validatePath(sourcePath);
+  await validatePath(destinationPath);
+
+  const brotliCompress = createBrotliCompress();
+
+  const sourceStream = createReadStream(sourcePath);
+  const destinationStream = createWriteStream(destinationPath);
+
+  await pipeline(sourceStream, brotliCompress, destinationStream);
+};
---
+++ src/commands/decompress.mjs
@@ -0,0 +1,24 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { join } from 'node:path';
+import { pipeline } from 'node:stream/promises';
+import { createBrotliDecompress } from 'node:zlib';
+
+import { validatePath } from '../utils.mjs';
+
+export const decompress = async ({ passedProps, currentWorkingDirectory }) => {
+  const pathToFile = passedProps[0];
+  const pathToDestination = passedProps[1];
+
+  const sourcePath = join(currentWorkingDirectory, pathToFile);
+  const destinationPath = join(currentWorkingDirectory, pathToDestination);
+
+  await validatePath(sourcePath);
+  await validatePath(destinationPath);
+
+  const brotliDecompress = createBrotliDecompress();
+
+  const sourceStream = createReadStream(sourcePath);
+  const destinationStream = createWriteStream(destinationPath);
+
+  await pipeline(sourceStream, brotliDecompress, destinationStream);
+};
