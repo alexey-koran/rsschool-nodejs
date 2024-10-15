78570e7913393cbcdae3eee7c7fcb0349e313d40
Alexey Koran <alexey.koran@gmail.com>
2024-10-15 06:42:31 +0400

    feat: implement `up.mjs`

---
+++ src/commands/up.mjs
@@ -0,0 +1,15 @@
+import { homedir } from 'node:os';
+import { join } from 'node:path';
+
+import { programErrors } from '../messages.mjs';
+
+export const up = ({ currentWorkingDirectory, changeCurrentWorkingDirectory }) => {
+  const newPath = join(currentWorkingDirectory, '..');
+  const homeDir = homedir();
+
+  if (!newPath.startsWith(homeDir)) {
+    throw new Error(`${programErrors.invalidInput}. You can't go upper than root directory.`);
+  }
+
+  changeCurrentWorkingDirectory(newPath);
+};
