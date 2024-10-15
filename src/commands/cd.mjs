38e2531e0e4d90085364f3e2244f59079d4f13fa
Alexey Koran <alexey.koran@gmail.com>
2024-10-15 07:07:50 +0400

    fix: cd to not existing paths

--- src/commands/cd.mjs
+++ src/commands/cd.mjs
@@ -1,9 +1,10 @@
+import { access, constants } from 'node:fs/promises';
 import { homedir } from 'node:os';
 import { join } from 'node:path';
 
 import { programErrors } from '../messages.mjs';
 
-export const cd = ({
+export const cd = async ({
   properties: pathToDirectory,
   currentWorkingDirectory,
   changeCurrentWorkingDirectory,
@@ -13,6 +14,12 @@ export const cd = ({
 
   if (!newPath.startsWith(homeDir)) {
     throw new Error(`${programErrors.invalidInput}. You can't go upper than root directory.`);
+  }
+
+  try {
+    await access(newPath, constants.F_OK);
+  } catch (error) {
+    throw new Error(`${programErrors.invalidInput}. ${error?.message}`);
   }
 
   changeCurrentWorkingDirectory(newPath);
