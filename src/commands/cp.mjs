f1c50d95aa5fcd61152405ea9a432e6b9ee18945
Alexey Koran <alexey.koran@gmail.com>
2024-10-15 21:06:20 +0400

    feat: implement `cp.mjs`

---
+++ src/commands/cp.mjs
@@ -0,0 +1,16 @@
+import { cp as fsCP } from 'node:fs/promises';
+import { join } from 'node:path';
+
+export const cp = async ({ passedProps, currentWorkingDirectory }) => {
+  const pathToFile = passedProps[0];
+  const pathToNewDirectory = passedProps[1];
+
+  const sourcePath = join(currentWorkingDirectory, pathToFile);
+  const destinationPath = join(currentWorkingDirectory, pathToNewDirectory);
+
+  await fsCP(sourcePath, destinationPath, {
+    errorOnExist: true,
+    force: false,
+    recursive: true,
+  });
+};
