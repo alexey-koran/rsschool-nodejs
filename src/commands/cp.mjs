a8bb2d70c1d5aecec45666afe36b1df470cdd77f
Alexey Koran <alexey.koran@gmail.com>
2024-10-15 21:10:37 +0400

    fix: add paths validation to `cp.mjs`

--- src/commands/cp.mjs
+++ src/commands/cp.mjs
@@ -1,12 +1,17 @@
 import { cp as fsCP } from 'node:fs/promises';
 import { join } from 'node:path';
 
+import { validatePath } from '../utils.mjs';
+
 export const cp = async ({ passedProps, currentWorkingDirectory }) => {
   const pathToFile = passedProps[0];
   const pathToNewDirectory = passedProps[1];
 
   const sourcePath = join(currentWorkingDirectory, pathToFile);
   const destinationPath = join(currentWorkingDirectory, pathToNewDirectory);
+
+  await validatePath(sourcePath);
+  await validatePath(destinationPath);
 
   await fsCP(sourcePath, destinationPath, {
     errorOnExist: true,
