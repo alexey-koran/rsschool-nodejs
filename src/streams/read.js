8173d9937c2a1d6a6b6b6b7b0e4ba0f562978e3e
Alexey Koran <alexey.koran@gmail.com>
2024-10-20 21:14:55 +0400

    refactor: streams read task, use `pipeline`

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,12 +1,13 @@
 import { createReadStream } from 'node:fs';
 import { stdout } from 'node:process';
+import { pipeline } from 'node:stream/promises';
 
 const read = async () => {
   const currentDirname = import.meta.dirname;
 
   const input = createReadStream(`${currentDirname}/files/fileToRead.txt`);
 
-  input.pipe(stdout);
+  await pipeline(input, stdout);
 };
 
 await read();
