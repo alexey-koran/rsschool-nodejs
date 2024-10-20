b475fc4627e38fd53681e2921e40a4b4d88aadd7
Alexey Koran <alexey.koran@gmail.com>
2024-10-20 21:15:44 +0400

    refactor: streams write task, use `pipeline`

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,12 +1,13 @@
 import { createWriteStream } from 'node:fs';
 import { stdin } from 'node:process';
+import { pipeline } from 'node:stream/promises';
 
 const write = async () => {
   const currentDirname = import.meta.dirname;
 
   const writeableStream = createWriteStream(`${currentDirname}/files/fileToWrite.txt`);
 
-  stdin.pipe(writeableStream);
+  await pipeline(stdin, writeableStream);
 };
 
 await write();
