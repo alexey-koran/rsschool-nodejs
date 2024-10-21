761e8ffd4974bd1276029eb4d95be4f6f6560f27
Alexey Koran <alexey.koran@gmail.com>
2024-10-21 21:04:56 +0400

    feat: implement `snapshot-testing` tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,40 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+
+const firstValues = [1, 2, 3, 4, 5];
+const secondValues = [6, 7, 8, 9, 10];
+
+const expectedFirstLinkedList = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: {
+          value: 5,
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    },
+  },
+};
 
 describe('generateLinkedList', () => {
+  const firstLinkedList = generateLinkedList<number>(firstValues);
+  const secondLinkedList = generateLinkedList<number>(secondValues);
+
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(firstLinkedList).toStrictEqual(expectedFirstLinkedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(secondLinkedList).toMatchSnapshot('secondLinkedList');
   });
 });
