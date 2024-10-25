2815a1d40f5c58d7519d850a0a8a1d4650656c82
Alexey Koran <alexey.koran@gmail.com>
2024-10-25 21:24:12 +0400

    feat: update `table-tests` to cover more cases, refactor

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,28 +1,236 @@
 // Uncomment the code below and write your tests
 import { simpleCalculator, Action } from './index';
 
+interface ValidTestCase {
+  a: number;
+  b: number;
+  action: Action;
+  expected: number;
+}
+
+interface RawTestCase {
+  a: unknown;
+  b: unknown;
+  action: unknown;
+  expected: unknown;
+}
+
+const calcAddition = (a: number, b: number) => a + b;
+
+const additionTestCases: ValidTestCase[] = [
+  {
+    a: 1,
+    b: 2,
+    action: Action.Add,
+    expected: calcAddition(1, 2),
+  },
+  {
+    a: -5,
+    b: 6,
+    action: Action.Add,
+    expected: calcAddition(-5, 6),
+  },
+  {
+    a: 6,
+    b: -5,
+    action: Action.Add,
+    expected: calcAddition(6, -5),
+  },
+  {
+    a: -5,
+    b: -6,
+    action: Action.Add,
+    expected: calcAddition(-5, -6),
+  },
+];
+
+const calcSubtraction = (a: number, b: number) => a - b;
+
+const subtractionTestCases: ValidTestCase[] = [
+  {
+    a: 1,
+    b: 2,
+    action: Action.Subtract,
+    expected: calcSubtraction(1, 2),
+  },
+  {
+    a: -5,
+    b: 6,
+    action: Action.Subtract,
+    expected: calcSubtraction(-5, 6),
+  },
+  {
+    a: 6,
+    b: -5,
+    action: Action.Subtract,
+    expected: calcSubtraction(6, -5),
+  },
+  {
+    a: -5,
+    b: -6,
+    action: Action.Subtract,
+    expected: calcSubtraction(-5, -6),
+  },
+];
+
+const calcMultiplication = (a: number, b: number) => a * b;
+
+const multiplicationTestCases: ValidTestCase[] = [
+  {
+    a: 1,
+    b: 2,
+    action: Action.Multiply,
+    expected: calcMultiplication(1, 2),
+  },
+  {
+    a: -5,
+    b: 6,
+    action: Action.Multiply,
+    expected: calcMultiplication(-5, 6),
+  },
+  {
+    a: 6,
+    b: -5,
+    action: Action.Multiply,
+    expected: calcMultiplication(6, -5),
+  },
+  {
+    a: -5,
+    b: -6,
+    action: Action.Multiply,
+    expected: calcMultiplication(-5, -6),
+  },
+];
+
+const calcDivision = (a: number, b: number) => a / b;
+
+const divisionTestCases: ValidTestCase[] = [
+  {
+    a: 1,
+    b: 2,
+    action: Action.Divide,
+    expected: calcDivision(1, 2),
+  },
+  {
+    a: -5,
+    b: 6,
+    action: Action.Divide,
+    expected: calcDivision(-5, 6),
+  },
+  {
+    a: 6,
+    b: -5,
+    action: Action.Divide,
+    expected: calcDivision(6, -5),
+  },
+  {
+    a: -5,
+    b: -6,
+    action: Action.Divide,
+    expected: calcDivision(-5, -6),
+  },
+];
+
+const calcExponentiation = (a: number, b: number) => Math.pow(a, b);
+
+const exponentiationTestCases: ValidTestCase[] = [
+  {
+    a: 1,
+    b: 2,
+    action: Action.Exponentiate,
+    expected: calcExponentiation(1, 2),
+  },
+  {
+    a: -5,
+    b: 6,
+    action: Action.Exponentiate,
+    expected: calcExponentiation(-5, 6),
+  },
+  {
+    a: 6,
+    b: -5,
+    action: Action.Exponentiate,
+    expected: calcExponentiation(6, -5),
+  },
+  {
+    a: -5,
+    b: -6,
+    action: Action.Exponentiate,
+    expected: calcExponentiation(-5, -6),
+  },
+];
+
+const invalidTestCases: RawTestCase[] = [
+  {
+    a: 5,
+    b: -1,
+    action: 'invalidAction',
+    expected: null,
+  },
+  {
+    a: null,
+    b: 2,
+    action: Action.Add,
+    expected: null,
+  },
+  {
+    a: 1,
+    b: null,
+    action: Action.Add,
+    expected: null,
+  },
+  {
+    a: undefined,
+    b: 2,
+    action: Action.Add,
+    expected: null,
+  },
+  {
+    a: 1,
+    b: undefined,
+    action: Action.Add,
+    expected: null,
+  },
+  {
+    a: '1',
+    b: 2,
+    action: Action.Add,
+    expected: null,
+  },
+  {
+    a: 1,
+    b: '2',
+    action: Action.Add,
+    expected: null,
+  },
+  {
+    a: '1',
+    b: '2',
+    action: Action.Add,
+    expected: null,
+  },
+  {
+    a: 'a',
+    b: 'b',
+    action: Action.Add,
+    expected: null,
+  },
+];
+
 const testCases = [
-  { a: 1, b: 2, action: Action.Add, expected: 3 },
-  { a: 10, b: 5, action: Action.Subtract, expected: 5 },
-  { a: 20, b: 30, action: Action.Subtract, expected: -10 },
-  { a: 1, b: 2, action: Action.Multiply, expected: 2 },
-  { a: 2, b: -2, action: Action.Multiply, expected: -4 },
-  { a: 4, b: 2, action: Action.Divide, expected: 2 },
-  { a: 2, b: 4, action: Action.Divide, expected: 0.5 },
-  { a: 10, b: -5, action: Action.Divide, expected: -2 },
-  { a: 5, b: 2, action: Action.Exponentiate, expected: 25 },
-  { a: 5, b: 1, action: Action.Exponentiate, expected: 5 },
-  { a: 5, b: 0, action: Action.Exponentiate, expected: 1 },
-  { a: 5, b: -1, action: Action.Exponentiate, expected: 0.2 },
-  { a: 5, b: -1, action: 'invalidAction', expected: null },
-  { a: 'invalidArgument', b: -1, action: Action.Exponentiate, expected: null },
+  ...additionTestCases,
+  ...subtractionTestCases,
+  ...multiplicationTestCases,
+  ...divisionTestCases,
+  ...exponentiationTestCases,
+  ...invalidTestCases,
 ];
 
 describe('simpleCalculator', () => {
   test.each(testCases)(
     `Test case #$#. Parameters: a = $a, b = $b, action = $action | expected: $expected`,
-    (testCase) => {
-      expect(simpleCalculator(testCase)).toBe(testCase.expected);
+    ({ a, b, action, expected }) => {
+      expect(simpleCalculator({ a, b, action })).toBe(expected);
     },
   );
 });
