03f9d331175823fbbc82e7ed627c627a656c5555
Alexey Koran <alexey.koran@gmail.com>
2025-05-01 16:50:24 +0400

    refactor: `fs:create`, add cause to throw Error

--- src/fs/create.js
+++ src/fs/create.js
@@ -5,8 +5,8 @@ const fileContent = 'I am fresh and young';
 const create = async () => {
   try {
     await writeFile(`${import.meta.dirname}/files/fresh.txt`, fileContent, { flag: 'wx' });
-  } catch (_error) {
-    throw new Error('FS operation failed');
+  } catch (error) {
+    throw new Error('FS operation failed', { cause: error });
   }
 };
 
