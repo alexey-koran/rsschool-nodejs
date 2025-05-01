5a13d42e806a9f1d5ce5a4dff31ec3b043f06e2b
Alexey Koran <alexey.koran@gmail.com>
2025-05-01 16:26:54 +0400

    refactor: `streams\transform.js`, use `EOL` instead of `\n`

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,13 +1,14 @@
 import { stdin, stdout } from 'node:process';
 import { Transform } from 'node:stream';
 import { pipeline } from 'node:stream/promises';
+import { EOL } from 'node:os';
 
 const transform = async () => {
   const transformStream = new Transform({
     transform(buffer, encoding, next) {
       const input = buffer.toString().trim();
       const reversed = input.split('').reverse().join('');
-      transformStream.push(`${reversed}\n`);
+      transformStream.push(`${reversed}${EOL}`);
       next();
     },
   });
