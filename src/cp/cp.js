625f1b3da715a959cf312ae522b924d61918f9e4
Alexey Koran <alexey.koran@gmail.com>
2025-05-02 21:59:26 +0400

    refactor: `cp.js`, gracefully end processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,23 +1,18 @@
 import { fork } from 'node:child_process';
-import { stdin, stdout } from 'node:process';
+import { stdin, stdout, exit } from 'node:process';
 import { pipeline } from 'node:stream/promises';
 
 const spawnChildProcess = async (args) => {
-  const controller = new AbortController();
+  const childProcess = fork(`${import.meta.dirname}/files/script.js`, args, { silent: true });
 
-  const currentDirname = import.meta.dirname;
-
-  const childProcess = fork(`${currentDirname}/files/script.js`, [...args], { silent: true });
-
-  pipeline(stdin, childProcess.stdin);
-
-  pipeline(childProcess.stdout, stdout);
-
-  childProcess.on('error', (err) => {
-    throw new Error(err);
+  childProcess.on('exit', () => {
+    exit();
   });
 
-  controller.abort();
+  await Promise.all([
+    pipeline(stdin, childProcess.stdin),
+    pipeline(childProcess.stdout, stdout),
+  ]);
 };
 
 spawnChildProcess(['test1', 'test2', 'test3', 'test4', 'test5']);
