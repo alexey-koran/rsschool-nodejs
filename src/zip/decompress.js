a818d21b38c080641428968e6acb02891c2e2c03
Alexey Koran <alexey.koran@gmail.com>
2025-05-02 22:17:22 +0400

    refactor: `decompress.js`, use `cause` when throwing errors, remove redundant import constant

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -4,16 +4,14 @@ import { createUnzip } from 'node:zlib';
 
 const decompress = async () => {
   try {
-    const currentDirname = import.meta.dirname;
-
     const unGzip = createUnzip();
 
-    const source = createReadStream(`${currentDirname}/files/archive.gz`);
-    const destination = createWriteStream(`${currentDirname}/files/fileToCompress.txt`);
+    const source = createReadStream(`${import.meta.dirname}/files/archive.gz`);
+    const destination = createWriteStream(`${import.meta.dirname}/files/fileToCompress.txt`);
 
     await pipeline(source, unGzip, destination);
   } catch (error) {
-    throw new Error(error);
+    throw new Error('FS operation failed', { cause: error });
   }
 };
 
