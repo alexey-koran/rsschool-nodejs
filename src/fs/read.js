4e6536473d7db33527c5609eb636003e6c21edb3
Alexey Koran <alexey.koran@gmail.com>
2025-05-02 22:08:32 +0400

    refactor: `read.js`, use `cause` when throwing errors, remove redundant import constant

--- src/fs/read.js
+++ src/fs/read.js
@@ -2,19 +2,13 @@ import { readFile } from 'node:fs/promises';
 
 const read = async () => {
   try {
-    const currentDirname = import.meta.dirname;
-
-    const fileContent = await readFile(`${currentDirname}/files/fileToRead.txt`, {
+    const fileContent = await readFile(`${import.meta.dirname}/files/fileToRead.txt`, {
       encoding: 'utf8',
     });
 
     console.log(fileContent);
   } catch (error) {
-    if (error?.code === 'ENOENT') {
-      throw new Error('FS operation failed');
-    }
-
-    throw new Error(error);
+    throw new Error('FS operation failed', { cause: error });
   }
 };
 
