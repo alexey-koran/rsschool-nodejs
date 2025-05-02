dcc3a01a7970880365c549ae44d26ee56827042f
Alexey Koran <alexey.koran@gmail.com>
2025-05-02 22:05:54 +0400

    refactor: `delete.js`, use `cause` when throwing errors, remove redundant import constant

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -2,15 +2,9 @@ import { rm } from 'node:fs/promises';
 
 const remove = async () => {
   try {
-    const currentDirname = import.meta.dirname;
-
-    await rm(`${currentDirname}/files/fileToRemove.txt`);
+    await rm(`${import.meta.dirname}/files/fileToRemove.txt`);
   } catch (error) {
-    if (error?.code === 'ENOENT') {
-      throw new Error('FS operation failed');
-    }
-
-    throw new Error(error);
+    throw new Error('FS operation failed', { cause: error });
   }
 };
 
