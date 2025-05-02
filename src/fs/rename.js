445d28e0c2bed728a9e62a1449dad9d2b8a8441d
Alexey Koran <alexey.koran@gmail.com>
2025-05-02 22:11:13 +0400

    refactor: `rename.js`, use `cause` when throwing errors, remove redundant import constant

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,34 +1,28 @@
 import { rename as fsRename, access, constants } from 'node:fs/promises';
 
 const rename = async () => {
-  const currentDirname = import.meta.dirname;
-
   try {
     const hasProperFile = await access(
-      `${currentDirname}/files/properFilename.md`,
+      `${import.meta.dirname}/files/properFilename.md`,
       constants.R_OK | constants.W_OK,
     );
 
     if (hasProperFile === undefined) {
-      throw new Error('FS operation failed');
+      throw new Error('FS operation failed', { cause: 'File not found or improperly defined' });
     }
   } catch (error) {
     if (error?.code !== 'ENOENT') {
-      throw new Error(error);
+      throw new Error('FS operation failed', { cause: error });
     }
   }
 
   try {
     await fsRename(
-      `${currentDirname}/files/wrongFilename.txt`,
-      `${currentDirname}/files/properFilename.md`,
+      `${import.meta.dirname}/files/wrongFilename.txt`,
+      `${import.meta.dirname}/files/properFilename.md`,
     );
   } catch (error) {
-    if (error?.code === 'ENOENT') {
-      throw new Error('FS operation failed');
-    }
-
-    throw new Error(error);
+    throw new Error('FS operation failed', { cause: error });
   }
 };
 
