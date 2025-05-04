257dac7136e8e1f5cedd98b0d0f5363eb465a954
Alexey Koran <alexey.koran@gmail.com>
2025-05-05 03:28:57 +0400

    fix: streams `read.js`, remove successful message

--- src/streams/read.js
+++ src/streams/read.js
@@ -2,14 +2,11 @@ import { createReadStream } from 'node:fs';
 import { stdout } from 'node:process';
 import { pipeline } from 'node:stream/promises';
 import { join } from 'node:path';
-import { messages } from '../constants.mjs';
 
 const read = async () => {
   const input = createReadStream(join(import.meta.dirname, 'files', 'fileToRead.txt'));
 
   await pipeline(input, stdout);
-
-  console.log(messages.success, messages.streams.read);
 };
 
 await read();
