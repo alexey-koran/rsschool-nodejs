e38f4654e3eae4d356d84b8a1e85e4e0b7256f25
Alexey Koran <alexey.koran@gmail.com>
2025-05-04 20:57:29 +0400

    feat: add messages constant

---
+++ src/constants.mjs
@@ -0,0 +1,13 @@
+export const messages = {
+  fs: {
+    create: 'File created',
+    copy: 'Files copied',
+    read: 'File read',
+    list: 'Files listed',
+    remove: 'File removed',
+    rename: 'File renamed',
+    fileNotFound: 'File not found or improperly defined',
+  },
+  success: 'FS operation successful',
+  error: 'FS operation failed',
+};
