f2820647d5e9c2de55df4797923cbfe22cea1804
Alexey Koran <alexey.koran@gmail.com>
2025-05-05 03:28:29 +0400

    fix: streams `write.js`, remove successful message

--- src/streams/write.js
+++ src/streams/write.js
@@ -2,7 +2,6 @@ import { createWriteStream } from 'node:fs';
 import { stdin } from 'node:process';
 import { pipeline } from 'node:stream/promises';
 import { join } from 'node:path';
-import { messages } from '../constants.mjs';
 
 const write = async () => {
   const writeableStream = createWriteStream(
@@ -10,8 +9,6 @@ const write = async () => {
   );
 
   await pipeline(stdin, writeableStream);
-
-  console.log(messages.success, messages.streams.write);
 };
 
 await write();
