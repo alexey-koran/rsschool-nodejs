4874b54d755e432832c7deae1a8c8cdb1c402c56
Alexey Koran <alexey.koran@gmail.com>
2025-05-05 05:39:39 +0400

    Revert "chore: temp remove dev dependencies for cross-check"
    This reverts commit bc58e00b4becfa5def1e393c64a5c1359320e695.

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import {
+  baseConfig,
+  javaScriptConfig,
+  supportedFileTypes,
+  supportedExtensions,
+} from '@alexey-koran/eslint-config';
+
+const config = [
+  baseConfig,
+  javaScriptConfig,
+  {
+    files: [supportedFileTypes.js],
+    settings: {
+      'import/extensions': supportedExtensions.js,
+      'import/parsers': {
+        '@typescript-eslint/parser': [...supportedExtensions.ts, ...supportedExtensions.tsx],
+      },
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+        node: {
+          paths: ['src'],
+          extensions: supportedExtensions.js,
+        },
+      },
+    },
+  },
+  {
+    // The new config system no longer supports .eslintignore files
+    // https://eslint.org/blog/2022/10/eslint-v8.25.0-released/#highlights
+    ignores: ['build'],
+  },
+];
+
+export default config;
