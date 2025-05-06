3d2cba28cad1b7b766f6306d02021f5bba9c59dd
Alexey Koran <alexey.koran@gmail.com>
2025-05-06 18:46:09 +0400

    fix: `add` work with absolute paths

--- src/commands/add.mjs
+++ src/commands/add.mjs
@@ -3,6 +3,7 @@ import { join } from 'node:path';
 
 import { newFileName } from '../parameters/index.mjs';
 import { getCommandUsage } from '../utils/commandUsage.mjs';
+import { getPath } from '../validation/path.mjs';
 
 const parameters = {
   mandatory: [newFileName],
@@ -16,7 +17,9 @@ const help = {
 };
 
 const add = async ({ passedParameters: [_fileName], currentWorkingDirectory }) => {
-  await writeFile(join(currentWorkingDirectory, _fileName), '', { flag: 'wx' });
+  const newPath = getPath({ path: _fileName, currentWorkingDirectory });
+
+  await writeFile(join(currentWorkingDirectory, newPath), '', { flag: 'wx' });
 };
 
 export default {
